042d7e0fe26005a70cfdca6c5f650cca05aca236
comp211 <--global>
2021-01-15 00:37:03 +0200

    almost there

--- compiler.py
+++ compiler.py
@@ -2,27 +2,39 @@ import subprocess
 import os
 os.system('rm -rf testss')
 os.system('mkdir testss')
-for i in range(1,53):
-    cmd = "cat cases/{}.scm > infile; make ttt; ./test/test > testss/anss{}".format(i, i)
-    subprocess.check_output(cmd, shell=True)
-    cmd = 'scheme < cases/{}.scm >testss/sche{}'.format(i, i)
-    subprocess.check_output(cmd, shell=True)
-    with open('testss/sche{}'.format(i), 'r') as test_file:
-        found = False
-        with open('testss/anss{}'.format(i),'r') as ans_file:
-            for ans in ans_file.readlines():
-                for line in test_file.readlines():
-                    found = False
+nops = 'PC.X'
+def runs():
+    for i in range(1,53):
+        cmd = "cat cases/{}.scm > infile; make ttt>check; ./test/test > testss/anss{}".format(i, i)
+        subprocess.check_output(cmd, shell=True)
+        cmd = 'scheme < cases/{}.scm >testss/sche{}'.format(i, i)
+        subprocess.check_output(cmd, shell=True)
+        with open('test/test.s', 'r') as check:
+            if  len(check.readlines())==0:
+                print('+' * 50)
+                continue
 
-                    if ans in line:
-                        found = True
+        with open('testss/sche{}'.format(i), 'r') as test_file:
+            found = False
+            with open('testss/anss{}'.format(i),'r') as ans_file:
+                for ans in ans_file.readlines():
+                    for line in test_file.readlines():
+                        found = False
+
+                        if ans in line:
+                            found = True
+                            break
+                    if not found:
+                        print("test case {}.scm failed".format(i))
+                        print('OURS:\n\n')
+                        os.system('cat testss/anss{}'.format(i))
+                        print('-'*25)
+                        print('fucking SCHEME:\n\n')
+                        os.system('cat testss/sche{}'.format(i))
+                        print('+'*50)
                         break
-                if not found:
-                    print("test case {}.scm failed".format(i))
-                    print('OURS:\n\n')
-                    os.system('cat testss/anss{}'.format(i))
-                    print('-'*25)
-                    print('fucking SCHEME:\n\n')
-                    os.system('cat testss/sche{}'.format(i))
-                    print('+'*50)
-                    break
+                    else:
+                        print('test {} passed'.format(i))
+                        print('+' * 50)
+
+runs()
--- infile.scm
+++ infile.scm
@@ -1,9 +1,1 @@
-(define a (list 1 2 3 4))
-(set-car! a 0)
-a
-(set! a (list 0 0 0 0))
-a
-1
-2
-3
-4
+#{1}
